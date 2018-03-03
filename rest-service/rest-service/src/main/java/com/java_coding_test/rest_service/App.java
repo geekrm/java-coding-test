45295d4d1f6e2f4141a67053affeff8fdb1b9b3d
geekrm <geekrm@gmail.com>
2018-03-03 12:51:26 +0100

    Initial commit

---
+++ rest-service/rest-service/src/main/java/com/java_coding_test/rest_service/App.java
@@ -0,0 +1,13 @@
+package com.java_coding_test.rest_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
